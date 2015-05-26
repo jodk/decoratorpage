31ffb4a67627d7b25fd5a69e75f20228970ad573
jodk <kunde1@163.com>
2015-05-26 18:32:13 +0800

    file reader update

--- src/main/java/jodk/decoratorpage/reader/FileReader.java
+++ src/main/java/jodk/decoratorpage/reader/FileReader.java
@@ -1,6 +1,7 @@
 package jodk.decoratorpage.reader;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +14,28 @@ public class FileReader {
      * @return
      */
     public static List<File> files(String path){
-        //TODO
-        return  null;
+        File file = new File(path);
+        return files(file);
+    }
+    public static List<File> files(File file){
+        List<File> files = new ArrayList<File>();
+        if (file.exists()){
+            if(file.isDirectory()){
+                File[] dirs = file.listFiles();
+                for(File df : dirs){
+                    files.addAll(files(df));
+                }
+            }else {
+                files.add(file);
+            }
+        }
+        return  files;
+    }
+
+    public static void main(String[] args) {
+        List<File> fs = files("/home/supertool/IdeaProjects/decoratorpage");
+        for(File f : fs){
+            System.out.println(f.getAbsolutePath());
+        }
     }
 }
