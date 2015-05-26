a489fa50e8a918d8e025b80526ba639eba358c88
jodk <kunde1@163.com>
2015-05-26 17:28:48 +0800

    file reader

---
+++ src/main/java/jodk/decoratorpage/reader/FileReader.java
@@ -0,0 +1,19 @@
+package jodk.decoratorpage.reader;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by zhangdekun on 15-5-26-下午5:21.
+ */
+public class FileReader {
+    /**
+     * 查找这个路径下的所有文件
+     * @param path
+     * @return
+     */
+    public static List<File> files(String path){
+        //TODO
+        return  null;
+    }
+}
