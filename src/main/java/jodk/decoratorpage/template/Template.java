559556bac66d041f19624c43d1138d1059b85d46
jodk <kunde1@163.com>
2015-05-26 19:46:54 +0800

    test file

---
+++ src/main/java/jodk/decoratorpage/template/Template.java
@@ -0,0 +1,10 @@
+package jodk.decoratorpage.template;
+
+/**
+ * Created by zdk on 2015/5/26.
+ */
+public class Template {
+    public static final String T_START = "{%t:";
+    public static final String T_END= "t:%}";
+
+}
