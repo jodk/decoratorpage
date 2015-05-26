43bd1946f82130df509d91c1b4914ab6c7c3715d
jodk <kunde1@163.com>
2015-05-26 16:59:11 +0800

    init project

---
+++ src/main/java/jodk/decoratorpage/App.java
@@ -0,0 +1,13 @@
+package jodk.decoratorpage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
